334cdca0c16da461f16f24b44a78703aeeaa34ba
Roy Qu <royqh1979@gmail.com>
2019-06-01 23:09:22 +0800

    * work save

--- src/global_search.cpp
+++ src/global_search.cpp
@@ -37,13 +37,25 @@ int main() {
     vector<int> last_finished_in_jobs{}; // 每个job中已经完成的最后一个operation在job中的序号
     vector<int> step_jobs{}; //每一步选择执行哪个job中的第一个一个未执行operation
     vector<int> current_finish_times{}; //每一步之后当前图的完成时间
+    vector<int> remain_machine_times{}; //每个machine剩余任务的总完成时间
+    vector<int> remain_job_times{}; //每个job剩余任务的总完成时间
+
     int count=0;
 
+    for (int i=0;i<problem.machine_count();i++) {
+        remain_machine_times.push_back(0);
+    }
     for (int i=0;i<problem.job_count();i++) {
         last_operation_in_jobs.push_back(problem.operation_count_in_job(i)-1);
         last_finished_in_jobs.push_back(-1);
-
+        remain_job_times.push_back(0);
+        for (int j=0;j<problem.operation_count_in_job(i);j++) {
+            const POperation &op=problem.get_operation(i,j);
+            remain_job_times[i]+=op->duration();
+            remain_machine_times[op->machine_id()]+=op->duration();
+        }
     }
+
     int depth = 0;
     step_jobs.push_back(-1);
     while (depth >= 0) {
@@ -68,6 +80,8 @@ int main() {
             const POperation&  op = problem.get_operation(j,last_finished_in_jobs[j]);
             PJSPGraphNode& node = graph.get_node(op->id());
             graph.remove_last_node_in_machine_orders(node->machine_id);
+            remain_job_times[node->job_id]+=node->duration;
+            remain_machine_times[node->machine_id]+=node->duration;
             last_finished_in_jobs[j]--;
         } else {
 //            if (depth<=5)
@@ -80,13 +94,30 @@ int main() {
             const POperation&  op = problem.get_operation(j,last_finished_in_jobs[j]+1);
             PJSPGraphNode& node = graph.get_node(op->id());
             graph.set_node_as_next_in_machine_orders(node);
+            remain_job_times[node->job_id]-=node->duration;
+            remain_machine_times[node->machine_id]-=node->duration;
             graph.calc_node_earlist_times(node);
             //判断是否需要剪枝
 //            if (depth<=5)
 //                cout<<node->earliest_start_time<<"++"<<node->earliest_end_time<<"++"<<known_shortest_time<<endl;
-            if (node->earliest_end_time>=known_shortest_time) {
+            int max_last_time = 0;
+            for (int i=0;i<problem.job_count();i++) {
+                if (remain_job_times[i]>max_last_time) {
+                    max_last_time=remain_job_times[i];
+                }
+            }
+            for (int i=0;i<problem.machine_count();i++) {
+                if (remain_machine_times[i]>max_last_time) {
+                    max_last_time=remain_machine_times[i];
+                }
+            }
+//            int max_last_time = remain_job_times[node->job_id]>remain_machine_times[node->machine_id]?remain_job_times[node->job_id]:remain_machine_times[node->machine_id];
+            if (node->earliest_end_time+max_last_time>=known_shortest_time) {
                 //剪枝
+//                cout<<problem.operation_count()<<"cut:"<<depth<<","<<node->earliest_end_time+max_last_time<<endl;
                 graph.remove_last_node_in_machine_orders(node->machine_id);
+                remain_job_times[node->job_id]+=node->duration;
+                remain_machine_times[node->machine_id]+=node->duration;
                 continue;
             }
             //记录时间
@@ -108,6 +139,8 @@ int main() {
                     graph.generate_image(buf);
                 }
                 graph.remove_last_node_in_machine_orders(node->machine_id);
+                remain_job_times[node->job_id]+=node->duration;
+                remain_machine_times[node->machine_id]+=node->duration;
                 //回溯
                 step_jobs.pop_back();
                 depth--;
@@ -116,6 +149,8 @@ int main() {
                 const POperation&  op = problem.get_operation(j,last_finished_in_jobs[j]);
                 PJSPGraphNode& node = graph.get_node(op->id());
                 graph.remove_last_node_in_machine_orders(node->machine_id);
+                remain_job_times[node->job_id]+=node->duration;
+                remain_machine_times[node->machine_id]+=node->duration;
                 last_finished_in_jobs[j]--;
             } else {
                 //继续下一步
